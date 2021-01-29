22a5f4ee4a4a778ad1b2eedc8d1c9ddcc68677ae
Mikolaj Buczak <mikolaj.buczak@gmail.com>
2021-01-29 16:24:05 +0100

    Redirect to employeeDashboard after correct login

---
+++ scripts/loginEmployee.js
@@ -0,0 +1,45 @@
+// Autor: Mikołaj Buczak & Kamil Kaloch
+
+function addDigit(sender) {
+    let id = document.querySelectorAll('#id')[0].value;
+    
+    if (length(id) != 4) {
+        id = `${id}${sender.innerHTML}`
+        return;
+    }
+    
+    let pin = document.querySelectorAll('#pin')[0].value;
+    if (length(id) != 4) {
+        pin = `${pin}${sender.innerHTML}`
+
+        if (length(pin == 4)) {
+            loginEmployee();
+        }
+
+        return;
+    }
+
+}
+
+function loginEmployee() {
+    let id = document.querySelectorAll('#id')[0].value;
+    let pin = document.querySelectorAll('#pin')[0].value;
+
+    let credentials = `${id}:${pin}`;
+    credentials = btoa(credentials);
+    let hash = `Basic ${credentials}`;
+
+    let request = new XMLHttpRequest();
+    request.open("GET", "..\\..\\rejestrator\\api\\loginEmployee", false);
+    request.setRequestHeader("Authorization", hash);
+    request.send();
+
+    if (request.status != 200) {
+        let error = document.querySelectorAll(".error")[0];
+        error.classList.remove('error');
+        return;
+    }
+
+    window.location.replace("employeeDashboard.html");
+    console.log(request.responseText);
+}
