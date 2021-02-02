8d6b7eada600558d2121dcf40f82ec13a08b6913
Mikolaj Buczak <mikolaj.buczak@gmail.com>
2021-02-02 11:02:45 +0100

    Adds missing adminDashboard.js script

---
+++ scripts/adminDashboard.js
@@ -0,0 +1,19 @@
+// Autor: Mikołaj Buczak & Kamil Kaloch
+
+function create() {
+    let nameField = document.querySelectorAll('#name')[0];
+
+    let name = sessionStorage.getItem('currentAdminName');
+    let surname = sessionStorage.getItem('currentAdminSurname');
+
+    nameField.innerHTML = `${name} ${surname}`;
+}
+
+function logout() {
+    sessionStorage.removeItem('currentAdminID');
+    sessionStorage.removeItem('currentAdminPin');
+    sessionStorage.removeItem('currentAdminName');
+    sessionStorage.removeItem('currentAdminSurname');
+    
+    window.location.replace("loginAdmin.html");
+}
