20ebb01fc2f7028a940fb61a860e78c75df90079
Mikolaj Buczak <mikolaj.buczak@gmail.com>
2021-02-04 15:32:51 +0100

    Changes the logo to fixed REJESTRATOR string

--- scripts/adminDashboard.js
+++ scripts/adminDashboard.js
@@ -1,14 +1,5 @@
 // Autor: Mikołaj Buczak & Kamil Kaloch
 
-function create() {
-    let nameField = document.querySelectorAll('#name')[0];
-
-    let name = sessionStorage.getItem('currentAdminName');
-    let surname = sessionStorage.getItem('currentAdminSurname');
-
-    nameField.innerHTML = `Witaj ${name} ${surname}!`;
-}
-
 function logout() {
     sessionStorage.removeItem('currentAdminID');
     sessionStorage.removeItem('currentAdminPin');
